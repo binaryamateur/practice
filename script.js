7e34d4a4009ee5e58930ca32077b4349c188642e
Souvik Patra <souvikpatra@souvik.patra-C02DT3PXMD6N>
2023-01-31 14:28:55 +0530

    added button onclick and single DOM update for first time data load

--- script.js
+++ script.js
@@ -42,45 +42,34 @@ const list = document.querySelector(".list");
 const arrayStudents = JSON.parse(localStorage.getItem("arrayStudents")).data;
 
 let idx = 0;
-const studentList = arrayStudents.map(student => {
-    const newCard = document.createElement("div");
-    newCard.setAttribute("class", "card");
-    const profileImage = document.createElement("img");
-    profileImage.setAttribute("src", student.image);
-    profileImage.setAttribute("class", "profile-image");
-    newCard.append(profileImage);
-    const content = document.createElement("div");
-    content.setAttribute("class", "content");
-    content.innerHTML = `
-        Name: ${student.sname} <br> 
-        Address: ${student.address} <br>
-        Class: ${student.class}
-    `;
-    newCard.append(content);
-    const delButton = document.createElement("button");
-    delButton.setAttribute("id", idx++);
-    delButton.setAttribute("class", "delete-button");
-    delButton.innerHTML = "Delete";
-    delButton.addEventListener("click", function(event){
-        event.preventDefault();
-        console.log(event);
-        const studentList = JSON.parse(localStorage.getItem("arrayStudents")).data;
-        studentList.splice(event.target.id, 1);
-        localStorage.setItem("arrayStudents",JSON.stringify({
-            data: studentList,
-        }))
-        window.location.reload();
-    })
-    newCard.append(delButton);
-    list.append(newCard);
-    return {
-        sname: student.sname,
-        address: student.address,
-        class: student.class,
-        image: student.image,
-        card: newCard
-    }
-});
+
+let handleEvent = function(e){
+    console.log(e.id)
+    const studentList = JSON.parse(localStorage.getItem("arrayStudents")).data;
+    studentList.splice(e.id, 1);
+    localStorage.setItem("arrayStudents",JSON.stringify({
+        data: studentList,
+    }))
+    window.location.reload();
+}
+
+window.handleEvent = handleEvent;
+
+const studentList = arrayStudents.map((student,index) => {
+    return `
+        <div class = "card">
+            <img src = ${student.image} class = "profile-image"/>
+            <div class = "content">
+                Name: ${student.sname} <br> 
+                Address: ${student.address} <br>
+                Class: ${student.class}
+            </div>
+            <button id = ${index} class = "delete-button" onClick = "handleEvent(this)">Delete</button>
+        </div>
+        `;
+}).join("");
+
+list.innerHTML = studentList;
 
 function debounce(func, ms){
     let timeout;
@@ -90,7 +79,8 @@ function debounce(func, ms){
     };
 };
 
-function filter(e, studentList){
+function filter(e){
+    const studentList = JSON.parse(localStorage.getItem("arrayStudents")).data;
     const input = e.target.value.toLowerCase();
     const list = document.querySelector(".list");
     const filteredList = studentList.map((student, index) => {
@@ -106,9 +96,9 @@ function filter(e, studentList){
                     Address: ${student.address} <br>
                     Class: ${student.class}
                 </div>
-                <button id = ${index} class = "delete-button">Delete</button>
+                <button id = ${index} class = "delete-button" onClick = "handleEvent(this)">Delete</button>
             </div>
-            `
+            `;
         else{
             return "";
         }
@@ -118,6 +108,6 @@ function filter(e, studentList){
 
 const searchInput = document.querySelector("#search");
 let debouncedFilter = debounce(filter,1000);
-searchInput.addEventListener("input",(e) => debouncedFilter(e, studentList))
+searchInput.addEventListener("input",(e) => debouncedFilter(e))
 
 console.log(arrayStudents);
